9a1139881c25de18208f8403cdfdb0fc17b9f8ed
Raphael <graphael.dr@gmail.com>
2024-03-14 16:39:07 -0600

    make button component

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="onClick()" :style="{background:color}">{{ text }}</button>
+</template>
+
+<script>
+
+  export default {
+    name: 'Button',
+    props: {
+      text: String,
+      color: String
+    },
+    methods: {
+      onClick(){
+        console.log("Clicked " + this.text)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    color: white;
+  }
+
+  button:hover {
+    opacity: 0.9;
+  }
+</style>
