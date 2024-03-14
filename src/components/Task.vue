376c54ad88a2ba886a822952dc351a9291670701
Raphael <graphael.dr@gmail.com>
2024-03-14 17:00:50 -0600

    make task component

---
+++ src/components/Task.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>{{ task.title }}</h3>
+    <p>{{task.day}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Task',
+    props: {
+      task: Object
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
