fa1f3b2bea220b19790b491c2e81cd0283da5188
Raphael <graphael.dr@gmail.com>
2024-03-14 21:47:45 -0600

    place button in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,29 @@
 <template>
-  <header>{{ title }}</header>  
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="New Task" color="green"/>  
+  </header>
 </template>
 
 <script>
+import Button from './Button.vue'
   export default {
     name: 'Header',
     props: {
       title: String,
     },
+    components: {
+      Button
+    }
   }
 </script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+
+</style>
