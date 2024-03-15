43e2fa35bc0110b0e50795ed23417fe94cf9633d
Raphael <graphael.dr@gmail.com>
2024-03-14 21:48:11 -0600

    create add task form component

---
+++ src/components/AddTask.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Task name</p>
+    <input type='text' placeholder="Task name"/>
+    <p>Date</p>
+    <input type='text' placeholder="Date"/><br>
+    <span>Reminder</span>
+    <input type='checkbox' placeholder="Task name"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AddTask'
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px black solid;
+    margin: 0.5rem;
+    padding: 0.5rem;
+  }
+</style>
